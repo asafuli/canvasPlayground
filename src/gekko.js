30fa6f17b483f1f64d8a071c24cdb2f3d300a4e4
Assaf Eli <ASSAFEL@amdocs.com>
2018-10-21 11:05:49 +0300

    Initial commit

---
+++ src/gekko.js
@@ -0,0 +1,27 @@
+export default class Gekko {
+  
+  constructor(){
+    this.image = document.getElementById("gekkoImg");
+  }
+
+  draw(ctx){
+    ctx.drawImage(this.image, 0 , 0 ,180, 180 );
+  }
+
+  getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  
+  createRandMeasurements = () => {
+    return {x: Math.random() * 300, 
+            y: Math.random() * 300,
+            w: Math.random() * 1000,
+            h: Math.random() * 1000}
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,124 @@
+
+//Globals
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d");
+let gekkoImg = document.getElementById("gekkoImg");
+let catBoyImg = document.getElementById("catBoyImg");
+let lastTime = 0;
+let speed = 0;
+let movingSpeed = 50;
+let movingAxis = 'x';
+let basePosition = {
+  x : 200,
+  y : 200,
+  w : 180,
+  h : 180
+}
+let baseCatPosition = {
+  x : 400,
+  y : 200,
+  w : 180,
+  h : 180
+}
+let position = {...basePosition};
+let catPosition = {...baseCatPosition};
+let gekkoThumb = document.getElementById('gekkoThumb');
+
+//Functions
+drawGekko = ({x ,y ,w ,h}) => {
+  return ctx.drawImage(gekkoImg, x, y, w, h)
+}
+drawCatBoy = ({x ,y ,w ,h}) => {
+  return ctx.drawImage(catBoyImg, x, y, w, h)
+}
+
+move = (axis,sign) => {
+  speed = movingSpeed * sign;
+  movingAxis = axis;
+};
+
+jump = () => {
+  for (let i = 0; i < 20 ; i++){
+    setTimeout(() => {
+      ctx.clearRect(0, 0, 800, 500);
+      position.y = (i < 10) ? position.y - 10 : position.y + 10;
+      catPosition.y = (i < 10) ? catPosition.y - 10 : catPosition.y + 10;
+      drawGekko(position);
+      drawCatBoy(catPosition);
+    },i * 20,position, catPosition)
+  }
+}
+
+function moveGekko(e){
+  switch(e.key){
+    case 'ArrowRight':
+      move('x',1);
+      break
+    case 'ArrowLeft':
+      move('x',-1);
+      break
+    case 'ArrowUp':
+      move('y',-1);
+      break
+    case 'ArrowDown':
+      move('y',1);
+      break
+    case 'b' :
+      boost();
+      break
+    case ' ':
+      jump();
+      break
+  }
+}
+
+function stopGekko(e){
+  switch(e.key){
+    case 'ArrowRight':
+      if (speed > 0 && movingAxis === 'x') speed = 0;  
+      break
+    case 'ArrowLeft':
+      if (speed < 0 && movingAxis === 'x') speed = 0;
+      break
+    case 'ArrowUp':
+      if (speed < 0 && movingAxis === 'y') speed = 0;
+      break
+    case 'ArrowDown':
+      if (speed > 0 && movingAxis === 'y') speed = 0;
+      break
+  }
+}
+
+window.addEventListener('keydown', e => moveGekko(e));
+window.addEventListener('keyup', e => stopGekko(e));
+//players thumbnails
+
+/*gekkoThumb.addEventListener('click',() => {
+  ctx.clearRect(0, 50, 800, 500);
+  drawGekko(basePosition);
+  drawCatBoy(baseCatPosition);
+  drawCatBoy(catPosition);
+  drawGekko(position)
+},false);*/
+
+updateGekko = (deltaTime) => {
+  if (!deltaTime) return;
+  position[movingAxis] += speed/deltaTime;
+}
+
+function GameLoop(timestamp){
+  let dt = timestamp - lastTime;
+  lastTime = timestamp;
+
+  ctx.clearRect(0, 0, 800, 500);
+  updateGekko(dt);
+  drawGekko(position);
+
+  requestAnimationFrame(GameLoop);
+}
+//Initial call
+GameLoop();
+
+
+
+
